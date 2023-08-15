cc4db097147af001f7228c1b3140de9864d0caa1
Yu <kiras0518@gmail.com>
2023-08-15 17:44:57 +0800

    Initial Commit

---
+++ SelectedPhotosByExample/SelectedPhotosByExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectedPhotosByExampleApp.swift
+//  SelectedPhotosByExample
+//
+//  Created by yukun on 2023/8/15.
+//
+
+import SwiftUI
+
+@main
+struct SelectedPhotosByExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
